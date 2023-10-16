56fa7e01802ccbea944ac873fd3bb2504886f67a
pankaj maurya <pankajmaurya138@gmail.com>
2023-10-16 12:27:07 +0530

    app init

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading= React.createElement("h1",{id:"heading",className:"header"},"First Project in React");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading)
